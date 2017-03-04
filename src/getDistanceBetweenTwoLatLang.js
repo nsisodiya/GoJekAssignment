64d2b39c9791b789cd7b2182c308652685feba3c
Narendra Sisodiya <narendra@narendrasisodiya.com>
2017-03-04 16:52:56 +0530

    Added Distance calculation formula and its Test Cases.

---
+++ src/getDistanceBetweenTwoLatLang.js
@@ -0,0 +1,21 @@
+/**
+ * Created by narendrasisodiya on 04/03/17.
+ */
+//Copy Paste from  - http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
+module.exports = function getDistanceBetweenTwoLatLang(lat1, lon1, lat2, lon2) {
+  //Validation of Lat/Long is not done here. We assume, that we have got correct result.
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c * 1000; // Distance in m
+  return parseInt(d);
+};
---
+++ test/getDistanceBetweenTwoLatLang.test.js
@@ -0,0 +1,14 @@
+var chai, {expect} = require("chai");
+
+var getDistanceBetweenTwoLatLang = require("../src/getDistanceBetweenTwoLatLang");
+describe("Testing getDistanceBetweenTwoLatLang function", function () {
+  it("Should return 0 for identical lat/lang combo", function () {
+    expect(getDistanceBetweenTwoLatLang(20, 20, 20, 20)).to.equal(0);
+    expect(getDistanceBetweenTwoLatLang(30, 50, 30, 50)).to.equal(0);
+  });
+
+  it("Should return 1499099 for combinations 20, 20, 30, 30", function () {
+    expect(getDistanceBetweenTwoLatLang(20, 20, 30, 30)).to.equal(1499099);
+  });
+
+});
