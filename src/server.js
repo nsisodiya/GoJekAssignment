79135a79daa224cc880660862ee7a76e78f17f0b
Narendra Sisodiya <narendra@narendrasisodiya.com>
2017-03-04 23:28:54 +0530

    started server-api code using express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-require("./DriverLocation");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('GoJek Assignment By Narendra Sisodiya')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
